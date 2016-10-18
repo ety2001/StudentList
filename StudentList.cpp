8bdaa13def012fb0714a71f6b48136d27f47ab7f
Elaine Yang <299747@beaverton.k12.or.us>
2016-10-18 14:23:25 -0700

    another commit

--- StudentList.cpp
+++ StudentList.cpp
@@ -4,40 +4,74 @@
 #include <string>
 #include <string.h>
 #include <vector>
+
 using namespace std;
 
+void addentry(vector<Student*> *list);
+void deleteentry(vector<Student*> *list);
+void printentries(vector<Student*> *list);
+
 struct student{
-        char firstname [20];
-	char lastname [20];
+        char first [20];
+	char last [20];
 	int id;
 	float gpa;
+	for (int i = 0; i < 20; i++){{
+		firstname[i] = ' ';
+		lastname[i] = ' ';
+	}
 };
 
 
 int main(){
-	char stuname[20];
-
-	cout << "What is your command?" << endl;
-	char input [6];
-	for (int i = 0; i < 3; i++){
-		input[i] = ' ';
-	}
-	cin >> input;
+	bool run;
+	vector<Student*> *list = new vector<Student*>(80); 
 	
-	if (strcmp(input, "ADD") == true){
-		cout << "Name?" << endl;
-		cin.getline(stuname, sizeof(stuname));
-		int size = strlen(stuname);
+	while(run == true){
+		cout << "What is your command?" << endl;
+		char input [6];
+		for (int i = 0; i < 3; i++){
+			input[i] = ' ';
+		}
+		cin >> input;
 		
+		if (strcmp(input, "ADD   ") == true){
+			addentry(list);	
+		}
+		else if (strcmp(input, "DELETE") == true){
+			deleteentry(list);
+		}
+		else if (strcmp(input, "PRINT ") == true){
+			listentries(list);
+		}
+		else if (strcmp(input, "QUIT  ") == true){
+			//euiserhglrgsergusedfrj
+		}
+		else{
+			cout << "Please enter a valid command." << endl;
+		}
 	}
-	if (strcmp(input, "DELETE") == true){
-
-	}
-	if (strcmp(input, "LIST") == true){
-
-	}
-
-	cout << "me + me = ?" << endl;
-	cout << "it is meme!" << endl;
 }
 
+void addentry(vector<Student*> *list){
+	Student student;
+	cout << "Enter first name" << endl;
+	cin >> student.first;
+	cout << "Enter last name" << endl;
+	cin >> student.last;
+	cout << "Enter ID" << endl;
+	cin >> student.id;
+	cout << "Enter GPA" << endll
+	cin >> student.gpa;
+	list.push_back(student);
+}
+
+void deleteentry(vector<Student*> *list){
+	int iddelete;
+	cout << "Enter ID of student you want to delete" << endl;
+	cin >> iddelete;
+}
+
+void printentries(vector<Student*> *list){
+	cout << student.first << " " << student.last << ", " << student.id << ", " << student.gpa << endl;
+}
